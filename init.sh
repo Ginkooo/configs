c13f42ae2d5711ffa1182f0ec0b44f9609664e84
Piotr Czajka <czajka@protonmail.com>
2022-08-08 12:41:09 +0200

    adsfds

---
+++ init.sh
@@ -0,0 +1,7 @@
+ln -s configs/.vim ../.vim
+ln -s configs/.vim/.vimrc ../.vimrc
+
+ln -s configs/.zsh ../.zsh
+ln -s configs/.zsh/.zshrc ../.zshrc
+
+ln -s $HOME/configs/i3config $HOME/.config/i3/config
