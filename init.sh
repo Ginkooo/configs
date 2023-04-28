068952472e8aa1fdde7780f57d72873c2f15be45
Piotr Czajka <czajka@protonmail.com>
2023-04-28 11:39:15 +0200

    sdf

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
-ln -s configs/.vim ../.vim
-ln -s configs/.vim/.vimrc ../.vimrc
+ln -sf configs/.vim ../.vim
+ln -sf configs/.vim/.vimrc ../.vimrc
 
-ln -s configs/.zsh ../.zsh
-ln -s configs/.zsh/.zshrc ../.zshrc
+ln -sf configs/.zsh ../.zsh
+ln -sf configs/.zsh/.zshrc ../.zshrc
 
-ln -s $HOME/configs/i3config $HOME/.config/i3/config
+ln -sf $HOME/configs/i3config $HOME/.config/i3/config
