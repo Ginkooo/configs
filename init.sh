6de74f4d6081a07109f754a3d0a0c1d7490731c7
Piotr Czajka <piotr.czajka@stxnext.pl>
2023-09-30 22:17:10 +0200

    sdfds

--- init.sh
+++ init.sh
@@ -13,3 +13,6 @@ ln -sf $HOME/configs/i3config $HOME/.config/i3/config
 ln -sf $HOME/configs/init.vim $HOME/.config/nvim/init.vim
 
 ln -sf $HOME/configs/py3status $HOME/.config/py3status/config
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
